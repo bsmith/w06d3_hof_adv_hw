b6363b58b6067a2b9b702355462d59dcf6a699a6
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-23 20:27:33 +0000

    Try regular expressions.

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,11 +1,18 @@
+const sortLetters = str => str.toLowerCase().split('').sort().join('');
+
 const PangramFinder = function (phrase) {
-    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
     this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-    let phaseLetters = new Map(this.phrase.toLowerCase().split('').map(c => [c, true]));
-    return this.alphabet.every(letter => phaseLetters.has(letter));
+    // let phaseLetters = new Map(this.phrase.toLowerCase().split('').map(c => [c, true]));
+    // return this.alphabet.split('').every(letter => phaseLetters.has(letter));
+
+    const phaseSortedLetters = sortLetters(this.phrase);
+     /* remove duplicates and symbols */
+    const cleanedLetters = phaseSortedLetters.replaceAll(/([a-z])\1+|[^a-z]/g, '$1');
+    return cleanedLetters === this.alphabet;
 }
 
 module.exports = PangramFinder;
