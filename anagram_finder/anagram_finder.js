a22f192e41b8f30f7d06193629ecb106669eb16e
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-23 19:24:10 +0000

    Solve the Anagram Finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,14 @@
-const AnagramFinder = function (word) {
+const sortLetters = str => str.toLowerCase().split('').sort().join('');
 
+const AnagramFinder = function (word) {
+    this.word = word;
+    this.sortedLetters = sortLetters(word);
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    return otherWords.filter(word => {
+        return sortLetters(word) === this.sortedLetters && word !== this.word;
+    })
 }
 
 module.exports = AnagramFinder;
