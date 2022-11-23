8d61311d06a0450c8a02eb337b5a6664c020691c
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-23 18:54:01 +0000

    Solved Pangram Finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+    let phaseLetters = new Map(this.phrase.toLowerCase().split('').sort().map(c => [c, true]));
+    return this.alphabet.every(letter => phaseLetters.get(letter));
 }
 
 module.exports = PangramFinder;
