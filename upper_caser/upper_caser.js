b1462728175e3fe37d105b9b69ebd5401a8358aa
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-23 17:24:42 +0000

    Solve UpperCaser

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,15 @@
 const UpperCaser = function (words) {
-}
+    this.words = words;
+ };
 
+// UpperCaser.prototype.toUpperCase = function () {
+//     const wordToUpperCase = (word) => word.toUpperCase();
+//     return this.words.map(wordToUpperCase);
+// }
 UpperCaser.prototype.toUpperCase = function () {
-
+    /* wtf */
+    const wordToUpperCase = Function.prototype.call.bind(String.prototype.toUpperCase);
+    return this.words.map(wordToUpperCase);
 }
 
 module.exports = UpperCaser;
