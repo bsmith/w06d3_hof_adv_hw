4b1c436ffa15573b3f5c6785791cccdec7dce2ac
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-23 19:08:08 +0000

    Solved Isogram Finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,10 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    let letters = this.word.toLowerCase().split('').sort();
+    return letters.slice(1).every((letter, idx) => letters[idx] != letter);
 }
 
 module.exports = IsogramFinder;
