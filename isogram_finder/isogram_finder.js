ab6a7c313418d29c0c7f96c7c72a573dbdd774cf
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-24 11:18:15 +0000

    Set solution from Romain

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -3,8 +3,11 @@ const IsogramFinder = function (word) {
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-    const letters = this.word.toLowerCase().split('').sort();
-    return letters.slice(1).every((letter, idx) => letters[idx] != letter);
+    // const letters = this.word.toLowerCase().split('').sort();
+    // return letters.slice(1).every((letter, idx) => letters[idx] != letter);
+
+    const set = new Set(this.word.toLowerCase().split(''));
+    return set.size == this.word.length;
 
     // return !this.word.toLowerCase().match(/([a-z]).*\1/);
 }
